1f4142a0ebe74ac5426219056cfa0aff847eaf6b
Cuppa <gufranmm1@gmail.com>
2025-12-24 12:17:13 +0900

    Hapus beberapa file fungsi cetak struk

--- src/main/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-console.log("✅ PRELOAD AKTIF");
-
-contextBridge.exposeInMainWorld("printer", {
-  printReceipt: () => ipcRenderer.invoke("print-receipt")
-});
